1a1cc1cc6cbc3c1b69ef435d22f9b6f0aeb72ba7
Edlaine Zamora <edlainezamora@ip-192-168-0-12.ap-southeast-1.compute.internal>
2020-07-23 09:24:57 -0300

    Change the Header component creation to reduce the variability of creating components with a focus on learning

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './Header.css';
 
-function Header() {
-    return <header className="App-header">
-        <h1>React Session</h1>
-    </header>
+/* The ideal is to create this component as a function 
+and not as a class, as it is a simple component, 
+it does not have a State. */
+class Header extends React.Component {
+    render() {
+        return (
+            <header className="app-header">
+                <h1>React Session</h1>
+            </header>
+        )
+    }
 }
 
 export default Header;
